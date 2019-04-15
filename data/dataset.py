9b426c02a69f56f0ffdcff5fff561de95a05fef7
maggieezzat <maggieezzat9@gmail.com>
2019-04-15 15:07:10 +0200

    fixed a bug

--- data/dataset.py
+++ data/dataset.py
@@ -274,11 +274,11 @@ def input_fn(batch_size, deep_speech_dataset, repeat=1):
         ),
         output_shapes=(
             {
-                "features": tf.TensorShape([None, num_feature_bins, 1]),
+                "features": tf.TensorShape([100, num_feature_bins, 1]),
                 "input_length": tf.TensorShape([1]),
                 "label_length": tf.TensorShape([1]),
             },
-            tf.TensorShape([None]),
+            tf.TensorShape([100]),
         ),
     )
 
