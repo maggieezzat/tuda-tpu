11e0b4bd376aae0a6cc96e78670adbf0b130c2b8
maggieezzat <maggieezzat9@gmail.com>
2019-04-15 10:10:34 +0200

    fixed a bug

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install libasound2-dev
+sudo apt-get install python-dev python-numpy python-setuptools libsndfile-dev
+sudo apt-get install libasound2-dev
+
+pip3 install -r requirements.txt
