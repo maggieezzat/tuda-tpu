74f14a7d098098c20b297f66196d666a7738669c
maggieezzat <maggieezzat9@gmail.com>
2019-04-15 17:18:38 +0200

    trying sth

--- data/dataset.py
+++ data/dataset.py
@@ -25,7 +25,7 @@ import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import soundfile
 import tensorflow as tf
-import datasets
+from data.datasets import StreamingFilesDataset as sfd
 
 # pylint: enable=g-bad-import-order
 
