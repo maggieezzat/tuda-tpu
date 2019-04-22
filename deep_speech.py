243eb9f3ce61dab479e021051c7a726286a402a2
maggieezzat <maggieezzat9@gmail.com>
2019-04-22 12:33:30 +0200

    using bert optimizer

--- deep_speech.py
+++ deep_speech.py
@@ -205,12 +205,12 @@ def model_fn(features, labels, mode, params):
     optimizer = optimization.create_optimizer(
           loss, flags_obj.learning_rate, flags_obj.train_steps, 100 , flags_obj.use_tpu)
 
-    global_step = tf.train.get_or_create_global_step()
-    minimize_op = optimizer.minimize(loss, global_step=global_step)
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    #global_step = tf.train.get_or_create_global_step()
+    #minimize_op = optimizer.minimize(loss, global_step=global_step)
+    #update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     
     # Create the train_op that groups both minimize_ops and update_ops
-    train_op = tf.group(minimize_op, update_ops)
+    #train_op = tf.group(minimize_op, update_ops)
 
     return tf.contrib.tpu.TPUEstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
