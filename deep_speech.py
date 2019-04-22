61ef4e5ea4ef73b4d98d66c49309aacff5071ccd
maggieezzat <maggieezzat9@gmail.com>
2019-04-22 12:20:04 +0200

    fixed a bug

--- deep_speech.py
+++ deep_speech.py
@@ -196,8 +196,8 @@ def model_fn(features, labels, mode, params):
     # Compute CTC loss
     loss = tf.reduce_mean(ctc_loss(label_length, ctc_input_length, labels, probs))
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=flags_obj.learning_rate)
-    #optimizer = tf.train.AdaFactorOptimizer(learning_rate=flags_obj.learning_rate)
+    #optimizer = tf.train.AdamOptimizer(learning_rate=flags_obj.learning_rate)
+    optimizer = tf.train.AdaFactorOptimizer(learning_rate=flags_obj.learning_rate)
     if flags_obj.use_tpu:
       optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
     global_step = tf.train.get_or_create_global_step()
