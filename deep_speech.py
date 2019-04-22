803ae3f3b8e5e294f7a4f5c5fee602204c5fc3b6
maggieezzat <maggieezzat9@gmail.com>
2019-04-22 11:50:54 +0200

    fixed a bug

--- deep_speech.py
+++ deep_speech.py
@@ -77,6 +77,13 @@ def ctc_loss(label_length, ctc_input_length, labels, logits):
     #)
     y_pred = tf.log(tf.transpose(logits, perm=[1, 0, 2]) + tf.keras.backend.epsilon())
 
+    """tf.nn.ctc_loss_v2(
+            labels=labels,
+            logits=y_pred,
+            label_length=label_length,
+            logit_length=ctc_input_length,
+        ),
+    """
     return tf.expand_dims(
         tf.nn.ctc_loss(
             labels=labels,
@@ -85,12 +92,6 @@ def ctc_loss(label_length, ctc_input_length, labels, logits):
             ignore_longer_outputs_than_inputs=True,
         ),
         axis=1,
-        """tf.nn.ctc_loss_v2(
-            labels=labels,
-            logits=y_pred,
-            label_length=label_length,
-            logit_length=ctc_input_length,
-        ),"""
        
     )
 
