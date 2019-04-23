9f1634c432cecfe17eda923141cfe424013708d8
maggieezzat <maggieezzat9@gmail.com>
2019-04-23 14:05:55 +0200

    some changes

--- deep_speech.py
+++ deep_speech.py
@@ -230,7 +230,7 @@ def define_deep_speech_flags():
 
     flags_core.set_defaults(
         model_dir = "gs://deep_speech_bucket/german-speechdata-package-v2/deep_speech_modellayers5-nodes10-iterations50-steps1000/",
-        export_dir= "gs://deep_speech_bucket/german-speechdata-package-v2/deep_speech_saved_model/",
+        export_dir= "gs://deep_speech_bucket/german-speechdata-package-v2/deep_speech_saved_modellayers5-nodes10-iterations50-steps1000/",
         train_epochs=1,
         batch_size=8,
         hooks="",
