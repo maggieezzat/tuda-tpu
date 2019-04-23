c429d3f8a1f6746dcb50cad0c202e63ec0658d50
maggieezzat <maggieezzat9@gmail.com>
2019-04-23 16:10:05 +0200

    some changes

--- testopen.py
+++ testopen.py
@@ -7,10 +7,21 @@ import os
 from absl import app as absl_app
 from absl import flags
 import tensorflow as tf
+import logging
+import cloudstorage as gcs
+import webapp2
+
+from google.appengine.api import app_identity
 
 
-with open("gs://deep_speech_bucket/german-speechdata-package-v2/test.csv", 'r') as f:
-    content = f.readlines()
-    print(content[0])
-    print(content[1])
+
+def read_file(self, filename):
+  self.response.write('Reading the full file contents:\n')
+
+  gcs_file = gcs.open(filename)
+  contents = gcs_file.read()
+  print(contents)
+  gcs_file.close()
+
+read_file("gs://deep_speech_bucket/german-speechdata-package-v2/test.csv")
 
